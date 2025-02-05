909a0c611ba6ccd38c0a98a7454002d67cb2c716
Yea-Na <tlsdpsk1992@naver.com>
2025-02-06 05:47:47 +0900

    feat : 관심상품 희망 최저가 API 구현

--- myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -1,12 +1,10 @@
 package com.sparta.myselectshop.controller;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +18,12 @@ public class ProductController {
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
         // 응답 보내기
         return productService.createProduct(requestDto);
+    }
+
+    // 관심 상품 희망 최저가 등록하기
+    @PutMapping("/products/{id}")
+    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+        // 응답 보내기
+        return productService.updateProduct(id, requestDto);
     }
 }
