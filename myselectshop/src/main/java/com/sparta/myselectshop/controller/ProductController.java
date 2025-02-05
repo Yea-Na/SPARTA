ca006b73af13a9f12f215251279501cb6e3facb7
Yea-Na <tlsdpsk1992@naver.com>
2025-02-06 06:25:04 +0900

    feat : Scheduler 구현

--- myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -3,15 +3,18 @@ package com.sparta.myselectshop.controller;
 import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class ProductController {
 
-    private final com.sparta.myselectshop.service.productService productService;
+    private final ProductService productService;
 
     // 관심 상품 등록하기
     @PostMapping("/products")
@@ -25,5 +28,12 @@ public class ProductController {
     public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
         // 응답 보내기
         return productService.updateProduct(id, requestDto);
+    }
+
+    // 관심 상품 조회하기
+    @GetMapping("/products")
+    public List<ProductResponseDto> getProducts() {
+        // 응답 보내기
+        return productService.getProducts();
     }
 }
